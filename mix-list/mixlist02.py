4a49865af2a97cf3647644e99e234e9d151293cc
ab99NT <ab99NT@users.noreply.github.com>
2021-10-18 18:58:23 +0000

    mixed lists

---
+++ mix-list/mixlist02.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# create a list containing three items
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+print(f"IP addresses: {iplist[3]}, and {iplist[4]}")
+
+
