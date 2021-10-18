2012372cd0dfc9baa3380b02243bf17b94fe6496
ab99NT <ab99NT@users.noreply.github.com>
2021-10-18 16:26:02 +0000

    learning about input function

---
+++ lab_input/solution.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   print() - concatenate vs print a series of items"""
+
+def main():
+
+    # collect string input from the user
+    user_name = input("Please enter your name:")
+
+    day_week = input("Please enter the day of the week:")
+    
+    ## the line below creates a single string that is passed to print()
+    # print("You told me the IPv4 address is: " + user_input)
+    
+    ## print() can be given a series of objects separated by a comma
+    print("Hello,", user_name+"!"+" Happy "+day_week+"!")
+
+main()
