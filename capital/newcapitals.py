54e0e0586a1472cb4487dbf6ec277d75185411b6
ab99NT <ab99NT@users.noreply.github.com>
2021-10-19 20:23:46 +0000

    Whatever code

---
+++ capital/newcapitals.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+def main():
+
+    usr_country = input("Please enter a country to find it's capital:")
+
+    count = 0
+
+    while count < 3
+
+
+
+    if usr_country == "":
+       print("nice try")
+    else:
+       print("thanks for the value")
+
+
+    # Generate a dictionary from country list file.
+    countries = {}
+
+    try:
+        country_data = open('countries.txt', 'r')
+
+#        print("made it here")
+    except IOError:
+        print("Country list file could not be opened.")
+        quit(
+                )
+    try:
+        for each_line in country_data:
+            (country_name, capital_city) = each_line.split(':', 1)
+            country_name = country_name.strip()
+            capital_city = capital_city.strip()
+            countries[country_name] = capital_city  # "Japan": "Tokyo"
+        country_data.close()
+#        print( countries.values() )
+
+
+#        return countries
+
+    except ValueError:
+        pass
+
+#        print( countries.keys() )
+    print(f" The capital City of {usr_country} is {countries.get(usr_country)}" )
+
+main()
