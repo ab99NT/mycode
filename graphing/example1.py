97bb5f07b16ffa71b4552c81d6f34cbe60f0b7c8
ab99NT <ab99NT@users.noreply.github.com>
2021-10-21 15:21:19 +0000

    My Graphing

---
+++ graphing/example1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env/ python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+
+plt.rcdefaults()
+fig, ax = plt.subplots()
+
+# Example data
+people = ('Andrew', 'Dick', 'Harry', 'Slim', 'Jim')
+y_pos = np.arange(len(people))
+performance = 3 + 10 * np.random.rand(len(people))
+error = np.random.rand(len(people))
+
+ax.barh(y_pos, performance, xerr=error, align='center')
+ax.set_yticks(y_pos)
+ax.set_yticklabels(people)
+ax.invert_yaxis()  # labels read top-to-bottom
+ax.set_xlabel('Performance')
+ax.set_title('How fast do you want to go today?')
+
+plt.show()
+
+# SAVE the graph locally
+plt.savefig("/home/student/mycode/graphing/horizontalbar.png")
+# Save to "~/static"
+plt.savefig("/home/student/static/horizontalbar.png")
+print("Graph created.")
