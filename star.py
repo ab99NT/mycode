c6d856c02036931cc923dfe4a0a56ad65ed8a8e4
ab99NT <ab99NT@users.noreply.github.com>
2021-10-21 15:05:42 +0000

    Learning to work with the matplotlib library

---
+++ star.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+num = 8
+num2 = 8
+
+
+for i in range(1,num+1):
+    for j in range(1,i+1):
+         print("*",end=" ")
+    print()
+
+for x in range(8,num2-1):
+    for y in range(1,x-1):
+         print("*",end=" ")
+    print()
